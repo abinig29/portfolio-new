ab44e5d78ca43ff22a1da14dd78b6c1a7fc2c4f9
Abel Nigus <abinig5@gmail.com>
2024-04-02 03:38:25 +0300

    add link

--- src/config/site.ts
+++ src/config/site.ts
@@ -4,7 +4,7 @@ export const siteConfig = {
   name: "Abel's Portfolio",
   description:
     "Portfolio project containing projects that abel has been participating and build from scratch",
-  url: "https://portfolio-abel-site.vercel.app/",
+  url: "https://abel-portfolio-site.vercel.app/",
   links: {
     github: "https://github.com/abinig29/new",
   },
